72ca4370fd2944ca8cb238617980878e9c0dd8b9
Ajata Reddy <ajata.reddy@gmail.com>
2022-12-06 09:57:26 -0600

    Update api

--- resources/daily_illini.py
+++ resources/daily_illini.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
-import urllib2, json
+import json, urllib.request
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 base_url = 'http://dailyillini.com/category/'
@@ -8,7 +9,7 @@ def scraper(url):
     retval = {}
     for i in range(1, 6):
         try:
-            request = urllib2.urlopen(urllib2.Request(url + 'page/' + str(i), None, {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}))
+            request = urlopen(urllib.request.Request(url + 'page/' + str(i), None, {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}))
         except urllib2.HTTPError:
             break
         soup = BeautifulSoup(request, 'html.parser')
@@ -54,15 +55,44 @@ class SportsNews(Resource):
 class RecentNews(Resource):
     def get(self):
         request_url = 'http://dailyillini.com/feed/'
-        request = urllib2.urlopen(urllib2.Request(request_url, None, {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}))
+        request = urlopen(urllib.request.Request(request_url, None, {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}))
         soup = BeautifulSoup(request, 'lxml')
         retval = []
-        for x in soup.find_all('item'):
+        first = True
+        for x in soup.find_all('item'):            
             ret = {}
             ret['title'] = x.title.string
             ret['link'] = x.a['href']
             ret['date'] = x.contents[7].string
+
+            ret['desc'] = x.description.get_text().split("...")[0] + "..."
+
+            img = x.description.a.find('img')
+            if img != None:
+                try:
+                    srcset = img['srcset']
+                    srcset = srcset.split(", ")
+                    nums = []
+                    links = []
+                    maxVal = 0
+                    maxIndex = 0
+                    for index,src in enumerate(srcset):
+                        splitSrc = src.split(' ')
+                        links.append(splitSrc[0])
+                        num = int(splitSrc[1][:-1])
+                        nums.append(num)
+                        if num > maxVal:
+                            maxVal = num
+                            maxIndex = index
+                    ret['img_url'] = links[maxIndex]
+                except:
+                    print("failed to get image")
+
+            list_p = x.find('content:encoded').find_all('p')
+            content_list = [s.get_text() for s in list_p]
+            ret['content'] = content_list[:-3]
             retval.append(ret)
+
         return {'data':retval}
 
 
