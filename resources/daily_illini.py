24ead3dac3c1d3476ab830ccaec8aabbfa41d455
Ajata Reddy <ajata.reddy@gmail.com>
2022-12-12 12:10:02 -0600

    remove unused imports

--- resources/daily_illini.py
+++ resources/daily_illini.py
@@ -1,38 +1,7 @@
 from flask_restful import Resource
-import json, urllib.request
+import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
-def scraper(url):
-    retval = {}
-    for i in range(1, 6):
-        try:
-            request = urlopen(urllib.request.Request(url + 'page/' + str(i), None, {'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'}))
-        except urllib2.HTTPError:
-            break
-        soup = BeautifulSoup(request, 'html.parser')
-        retlist = []
-        for j in range(len(soup.find_all(class_='sno-animate')) - 4):
-            x = soup.find_all(class_='sno-animate')[j]
-            ret = {}
-            ret['title'] = x.h1.a.string
-            ret['link'] = x.h1.a['href']
-            if (x.p.contents[1].name != u'a'):
-                ret['date'] = x.p.contents[0]
-                ret['journalist'] = None
-            else :
-                ret['journalist'] = x.p.contents[1].string
-                if (x.p.contents[2].name is None):
-                    ret['date'] = x.p.contents[4]
-                else:
-                    ret['date'] = x.p.contents[3]
-            if (len(x.p.next_sibling.contents) == 0):
-                ret['partialtext'] = None
-            else:
-                ret['partialtext'] = x.p.next_sibling.contents[0].string
-            retlist.append(ret)
-        retval['Page ' + str(i)] = retlist
-    return retval
 
 class DailyIllini(Resource):
     def get(self):
@@ -76,7 +45,3 @@ class DailyIllini(Resource):
             retval.append(ret)
 
         return {'data':retval}
-
-
-if __name__ == '__main__':
-    scraper('http://dailyillini.com/category/news/champaign-urbana/')
